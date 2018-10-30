b83994d055a3f73d0b86cdc4c26350913387436e
Your Real Name <you@email.address>
2018-10-30 18:06:21 +0800

    vue-shop

---
+++ src/main.js
@@ -0,0 +1,39 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import store from './store'
+import {routerMode} from './config/env'
+import './config/rem'
+import FastClick from 'fastclick'
+
+if('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		FastClick.attach(document.body);
+	}, false)
+}
+
+Vue.use(VueRouter)
+const router = VueRouter({
+	router,
+	mode: routerMode,
+	strict: process.env.NODE_ENV !== 'production',
+	scrollBehavior (to, from, savedPosition}) {
+		if(savedPosition) {
+			return savedPosition
+		}else {
+			if(from.meta.keepAlive) {
+				from.meta.savedPosition = document.body.scrollTop;
+			}
+			return { x: 0, y: to.meta.savedPosition}
+		}
+	}
+})
+//Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  store,
+}).$mount('#app')
